b299776cc3ca8f1e68c82381192e79ee84ef58eb
Amar Ćatović <amar.catovic@isatis.com>
2023-03-04 15:08:38 +0100

    dodana postavka

---
+++ Aplikacija/Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assemmbly = typeof(DependencyInjection).Assembly;
+
+            services.AddMediatR(config => 
+                config.RegisterServicesFromAssembly(assemmbly));
+
+            services.AddValidatorsFromAssembly(assemmbly);
+
+            return services;
+        }
+    }
+}
---
+++ Aplikacija/WebApi/Program.cs
@@ -0,0 +1,32 @@
+using Application;
+using Infrastructure;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.Run();
